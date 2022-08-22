6da97e6acd90fcfe49e5648cdbb3d66515e413d4
Jeny Mazo <yenymzo@gmail.com>
2022-08-22 02:05:48 +0200

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import fetch from 'node-fetch';
 import minimist from 'minimist';
 import storage from 'node-persist';
-import { Holiday, CountryCode } from './types.js';
+import 'reflect-metadata';
+import { PersistDataService } from './service/storage-service/PersistDataService.js';
+import { DateNagerService } from './service/date-nager-service/DateNagerService.js';
+import { SanitizeHolidays } from './business/sanitizeHolidays/SanitizeHolidays.js';
+import { HolidayStorage } from './types.js';
 
 const options = minimist(process.argv.slice(2));
 
@@ -10,60 +13,36 @@ if (!options.countryCode) {
   process.exit(1)
 }
 
-const getUpcomingHolidays = async (countryCode: string): Promise<Holiday[]> => {
-  try {
-    const response = await fetch(`https://date.nager.at/api/v3/nextPublicholidays/${countryCode}`);
-    const data = await response.json() as Holiday[];
-    return data?.slice(0, 5);
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-}
+async function start(): Promise<void> {
+  const persistDataService = new PersistDataService();
 
-const isValidcountryCode = async (countryCode: string): Promise<CountryCode | undefined> => {
-  const response = await fetch(`https://date.nager.at/api/v3/availableCountries`);
-  const data = await response.json() as CountryCode[];
-  const validCountryCode = data.find(code => code.countryCode === countryCode.toUpperCase());
-  return validCountryCode;
-}
+  await persistDataService.init();
+  const fileStorage = await persistDataService.getData(options.countryCode);
 
-const formatHolidays = (holidays: Holiday[]) => {
-
-  const formattedHolidays = holidays.map(holiday => {
-    return {
-      date: holiday.date,
-      name: holiday.name,
-      counties: holiday.counties, // todo: format counties to show them all
-      types: holiday.types?.join(', '),
-    }
-  });
-  return formattedHolidays;
-}
-
-async function main() {
-  await storage.init({ttl: 10000});
-  const fileStorage = await storage.getItem(`data-${options.countryCode}`);
-  console.log('fileStorage', fileStorage);
-  let res: any;
+  let res: Pick<HolidayStorage, 'date' | 'name' | 'counties' | 'types'>[] = [];
 
   if (fileStorage) {
-    console.log('es valida');
     res = fileStorage;
   } else {
-    console.log('no es valida');
-    const validCountryCode = await isValidcountryCode(options.countryCode);
+    const dateNagerService = new DateNagerService();
+    const validCountryCode = await dateNagerService.isValidcountryCode(options.countryCode);
+
     if(!validCountryCode) {
       console.error(`"${options.countryCode}" is not a valid country code`);
       process.exit(1);
     } else {
-      const response = await getUpcomingHolidays(options.countryCode);
-      res = await formatHolidays(response);
+      const response = await dateNagerService.getUpcomingHolidays(options.countryCode);
+      const sanitizeHolidays = new SanitizeHolidays();
+
+      res = await sanitizeHolidays.formatHolidays(response);
       await storage.setItem(`data-${options.countryCode}`, res);
     }
     console.table(res);
   }
 }
 
-main();
+start().catch((err) => {
+  console.error(`Error starting server: ${err.message}`, err);
+  process.exit(-1);
+});
 
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,37 @@
 export type Holiday = {
-    date: string;
-    localName: string;
-    name: string;
-    countryCode: string;
-    fixed: boolean;
-    global: boolean;
-    counties: string[] | null;
-    launchYear: number | null;
-    types: holidayType[] | null;
+  date: string;
+  localName: string;
+  name: string;
+  countryCode: string;
+  fixed: boolean;
+  global: boolean;
+  counties: string[] | null;
+  launchYear: number | null;
+  types: holidayType[] | null;
 }
+
+type Modify<T, R> = Omit<T, keyof R> & R;
+
+export type HolidayStorage = Modify<Holiday,{
+  types: string | undefined;
+}>;
 
 export type CountryCode = {
-    countryCode: string;
-    name: string;
+  countryCode: string;
+  name: string;
 }
 
- enum holidayType {
-    Public,
-    Bank,
-    School,
-    Authorities,
-    Optional,
-    Observance
+export interface IDateNagerService {
+  getUpcomingHolidays(countryCode: string): Promise<Holiday[]>;
+  isValidcountryCode(countryCode: string): Promise<CountryCode | undefined>;
 }
+
+enum holidayType {
+  Public,
+  Bank,
+  School,
+  Authorities,
+  Optional,
+  Observance
+}
+
